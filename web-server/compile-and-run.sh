adcb773b513d204b1091e3f23166dfa2d4d20002
Jose <jos.alvatorre@gmail.com>
2019-03-03 15:54:47 -0800

    server kills process at port before starting
    The node server will kill whatever process exists at the specified port before starting. If this is not done, we'll get errors complaining about a pre-existing process at the port.
    
    A bash script was created to facilitate this process.

---
+++ web-server/compile-and-run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+tsc index.ts
+node index.js config.json
--- web-server/index.ts
+++ web-server/index.ts
@@ -1,10 +1,36 @@
-import { readFileSync } from 'fs';
 import * as express from 'express';
+import * as kill from 'kill-port';
+import { readFileSync } from 'fs';
+import { Config } from './Config';
 
-const config: object = JSON.parse(readFileSync(
-    './config.json', 'utf8'
+const [
+  node_filepath,
+  script_filepath,
+  config_filepath
+] = process.argv;
+
+// take configurations from config.json
+const config: Config = JSON.parse(readFileSync(
+  config_filepath, 'utf8'
 ));
 
+// create the express app
 const app: express.Express = express();
 
-console.log(config);
+// first, kill the process on the port.
+kill( config.port )
+.then(() => {
+  // then, start the server.
+  app.listen( config.port, () => {
+    console.log(`
+      The server defined in ${script_filepath}
+      is now running on port ${config.port}
+      using node ${node_filepath}
+    `);
+  });
+})
+.catch(() => {
+  console.log(`
+    failed to kill process at ${config.port}.
+  `);
+});
