ef9033838163816a592d8086f4d3b1bf6ee78944
Xin Luigi Chen <luxchen@ucdavis.edu>
2019-04-22 19:02:07 -0700

    plotting all access points in a building working

--- analytics-apps/test.py
+++ analytics-apps/test.py
@@ -1,9 +1,7 @@
-import helpers as hp
+import dataframe_manip as hp
 import pandas as pd
 
-data = hp.getData("wifi_data_until_20190204.csv")
+data = hp.csv_to_dataframe("wifi_data_until_20190204.csv")
 print('Successfully got data')
 
-y = len(hp.get_building_accesspoints(data, ''))
-
-print(y)
+print(hp.get_building_accesspoints(data, 'SCC'))
