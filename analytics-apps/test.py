e48689d500531ce7752cab483002b28618248611
Xin Luigi Chen <luxchen@ucdavis.edu>
2019-04-24 14:24:09 -0700

    functions in test.py

--- analytics-apps/test.py
+++ analytics-apps/test.py
@@ -4,4 +4,16 @@ import pandas as pd
 data = hp.csv_to_dataframe("wifi_data_until_20190204.csv")
 print('Successfully got data')
 
-print(hp.get_building_accesspoints(data, 'SCC'))
+scc_building = data[hp.get_building_accesspoints(data, 'SCC')].fillna(0).copy()
+
+scc_building['total'] = scc_building.sum(axis=1)
+
+scc_building.index = pd.to_datetime(scc_building.index, utc=True)
+
+scc_building = scc_building.resample('H').mean()
+
+scc_building['weekday'] = scc_building.index.weekday
+
+#monday = scc_building[['weekday'] == 0]
+
+print(scc_building)
